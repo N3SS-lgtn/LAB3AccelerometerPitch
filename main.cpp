2b0fa4a6468cfc614891bb963d783e2e289ff397
N3SS-lgtn <k-la-ipeda@hotmail.com>
2024-07-21 14:00:18 +0000

    Implemented pitch angle calculation using accelerometer

--- main.cpp
+++ main.cpp
@@ -1,41 +1,38 @@
-
 #include "mbed.h"
-#include "LSM6DSLSensor.h"
+#include "LIS3DSH.h"
 
-#define PI 3.141592654
+// Initialize the serial interface
+Serial pc(USBTX, USBRX);
 
-static DevI2C devI2c(PB_11,PB_10);
-static LSM6DSLSensor acc_gyro(&devI2c,0xD4,D4,D5); // high address
+// Initialize the I2C interface for the accelerometer
+I2C i2c(I2C_SDA, I2C_SCL);
 
+// Initialize the accelerometer
+LIS3DSH acc(i2c);
 
-float computeAngle(int x, int y, int z){
-    float res = 0;
-
-    return res;
+// Function to compute the pitch angle from accelerometer data
+float computeAngle(float x, float y, float z) {
+    // Calculate the pitch angle in degrees
+    float angle = atan2(y, sqrt(x * x + z * z)) * 180.0 / M_PI;
+    return angle;
 }
 
-/* Simple main function */
 int main() {
-    uint8_t id;
-    int32_t axes[3];
-    float res=0;
-    acc_gyro.init(NULL);
+    pc.printf("Starting accelerometer pitch calculation...\n");
 
-    acc_gyro.enable_x();
-    acc_gyro.enable_g();
+    while (true) {
+        // Read the accelerometer values
+        float x = acc.readX();
+        float y = acc.readY();
+        float z = acc.readZ();
 
-    printf("This is an accelerometer example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-    acc_gyro.read_id(&id);
-    printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
+        // Compute the pitch angle
+        float pitch = computeAngle(x, y, z);
 
-    while(1) {
+        // Print the pitch angle
+        pc.printf("Pitch angle: %.2f degrees\n", pitch);
 
-        acc_gyro.get_x_axes(axes);
-        res = computeAngle(axes[0], axes[1], axes[2]);
-        printf("LSM6DSL: %6d, %6d, %6d, %3.2f\r\n", axes[0], axes[1], axes[2], res);
-
-
-        thread_sleep_for(2000);
-
+        // Wait for 2 seconds
+        wait(2.0);
     }
 }
